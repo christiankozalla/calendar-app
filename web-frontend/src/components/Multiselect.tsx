934602327bc252a81967625701e41389d72c1dbe
Christian Kozalla <christian.kozalla@gmail.com>
2024-09-21 15:33:30 +0200

    refactor: multiselect usable inside form elements

--- web-frontend/src/components/Multiselect.tsx
+++ web-frontend/src/components/Multiselect.tsx
@@ -2,61 +2,66 @@ import { useState } from "react";
 import { Popover, Checkbox, Flex, Text, Button } from "@radix-ui/themes";
 import type { BaseModel } from "pocketbase";
 
-type MultiSelectProps<T extends BaseModel[]> = {
-	options: T;
+type MultiSelectProps<T extends BaseModel> = {
+	options: T[];
 	placeholder: string;
 	formfieldName: string;
 };
 
-export function MultiSelect<T extends BaseModel[]>({
+export function MultiSelect<T extends BaseModel>({
 	options,
 	placeholder,
 	formfieldName,
 }: MultiSelectProps<T>) {
 	const [open, setOpen] = useState(false);
-	// @ts-ignore
-	const [selectedOptions, setSelectedOptions] = useState<T>([]);
+	const [selectedOptions, setSelectedOptions] = useState<T[]>([]);
 
-	const handleToggle = (id: string) => {
-		setSelectedOptions((prev) => {
-			return prev.find((o) => o.id === id)
-				? (prev.filter((item) => item.id !== id) as T) // remove
-				: ([...prev!, options.find((o) => o.id === id)] as T); // add
-		});
+	const handleToggle = (option: T) => {
+		setSelectedOptions((prev) =>
+			prev.find((o) => o.id === option.id)
+				? prev.filter((item) => item.id !== option.id)
+				: [...prev, option],
+		);
 	};
 
 	return (
-		<Popover.Root open={open} onOpenChange={setOpen}>
-			<Popover.Trigger>
-				<Button variant="soft" className="w-full justify-between">
-					<span>
-						{selectedOptions?.length
-							? selectedOptions.map((o) => o.name).join(", ")
-							: placeholder}
-					</span>
-					<span className="rotate-90">&lsaquo;</span>
-				</Button>
-			</Popover.Trigger>
-			<Popover.Content className="w-[200px]">
-				<Flex gap="4">
-					{options.map((option) => (
-						<label
-							key={option.id}
-							htmlFor={option.id}
-							className="flex items-center space-x-2 cursor-pointer"
-						>
-							<Checkbox
-								id={option.id}
-								name={formfieldName}
-								value={option.id}
-								checked={selectedOptions?.some((o) => o.id === option.id)}
-								onCheckedChange={() => handleToggle(option.id)}
-							/>
-							<Text size="2">{option.name}</Text>
-						</label>
-					))}
-				</Flex>
-			</Popover.Content>
-		</Popover.Root>
-	);
+		<>
+			<Popover.Root open={open} onOpenChange={setOpen}>
+				<Popover.Trigger>
+					<Button variant="soft" className="w-full justify-between">
+						<span>
+							{selectedOptions.length
+								? selectedOptions.map((o) => o.name).join(", ")
+								: placeholder}
+						</span>
+						<span className="rotate-90">&lsaquo;</span>
+					</Button>
+				</Popover.Trigger>
+				<Popover.Content className="w-[200px]">
+					<Flex gap="4">
+						{options.map((option) => (
+							<label
+								key={option.id}
+								className="flex items-center space-x-2 cursor-pointer"
+							>
+								<Checkbox
+									checked={selectedOptions.some((o) => o.id === option.id)}
+									onCheckedChange={() => handleToggle(option)}
+								/>
+								<Text size="2">{option.name}</Text>
+							</label>
+						))}
+					</Flex>
+				</Popover.Content>
+			</Popover.Root>
+	selectedOptions.map((option) => (
+		<input
+			key={option.id}
+			type="hidden"
+			name={formfieldName}
+			value={option.id}
+		/>
+	));
+	</>
+	)
 }

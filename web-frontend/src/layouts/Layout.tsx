53bb59a0dba4cf86e4e17d94e0c9dbc311692ca9
Christian Kozalla <christian.kozalla@gmail.com>
2024-10-02 22:07:21 +0200

    remove Container

--- web-frontend/src/layouts/Layout.tsx
+++ web-frontend/src/layouts/Layout.tsx
@@ -1,14 +1,13 @@
 import { Outlet } from "react-router-dom";
-import { Container } from "@radix-ui/themes";
 import { SlidingDrawerManager } from "@/components/SlidingDrawerManager";
 
 export const Layout = () => {
 	return (
-		<Container size="2" py="9" px="2">
+		<>
 			<main>
 				<Outlet />
 			</main>
 			<SlidingDrawerManager />
-		</Container>
+		</>
 	);
 };

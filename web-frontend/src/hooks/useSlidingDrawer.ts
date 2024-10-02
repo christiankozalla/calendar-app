4648664bc34506fa30cfa4b7fcbe2ad6d8118d22
Christian Kozalla <christian.kozalla@gmail.com>
2024-10-02 22:09:49 +0200

    fmt

--- web-frontend/src/hooks/useSlidingDrawer.ts
+++ web-frontend/src/hooks/useSlidingDrawer.ts
@@ -8,20 +8,28 @@ export const useSlidingDrawer = () => {
 		const id = self.crypto.randomUUID();
 		setSlidingDrawerState((prev) => [...prev, { ...slide, id }]);
 
-		return id
+		return id;
 	};
 
 	const update = (slide: Pick<Slide, "id"> & Partial<Slide>) => {
 		setSlidingDrawerState((prev) => {
 			const slideToMergeWith = prev.find((s) => s.id === slide.id);
 			if (!slideToMergeWith) {
-				console.warn("Did not update SlidingDrawerState - Could not find id: ", slide.id);
+				console.warn(
+					"Did not update SlidingDrawerState - Could not find id: ",
+					slide.id,
+				);
 				return prev;
 			}
 			return [
 				...prev.filter((s) => s.id !== slide.id),
-				{ id: slideToMergeWith.id, props: { ...slideToMergeWith.props, ...slide.props }, state: { ...slideToMergeWith.state, ...slide.state }, component: slide.component || slideToMergeWith.component }, // updated slide, Note that the ordering might be relevant here!
-			]
+				{
+					id: slideToMergeWith.id,
+					props: { ...slideToMergeWith.props, ...slide.props },
+					state: { ...slideToMergeWith.state, ...slide.state },
+					component: slide.component || slideToMergeWith.component,
+				}, // updated slide, Note that the ordering might be relevant here!
+			];
 		});
 	};
 

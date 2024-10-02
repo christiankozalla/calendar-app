ece269a012ead1216f776bb5086c6c83abad4dd1
Christian Kozalla <christian.kozalla@gmail.com>
2024-10-02 22:09:32 +0200

    feat: use Swipable

--- web-frontend/src/components/SlidingDrawerManager.tsx
+++ web-frontend/src/components/SlidingDrawerManager.tsx
@@ -2,13 +2,31 @@ import { useRecoilValue } from "recoil";
 import { SlidingDrawerState } from "@/store/SlidingDrawerState";
 import { SlidingDrawer } from "./SlidingDrawer";
 import { useSlidingDrawer } from "@/hooks/useSlidingDrawer";
+import {
+	type OnSwipeParams,
+	Direction,
+	Swipable,
+	swipingDirection,
+} from "./Swipable";
 
 export const SlidingDrawerManager = () => {
 	const state = useRecoilValue(SlidingDrawerState);
 	const { update } = useSlidingDrawer();
 
+	const onSwipe = (directions: OnSwipeParams) => {
+		const swipingDirections = swipingDirection(directions);
+		if (swipingDirections.includes(Direction.DOWN)) {
+			for (const slide of state) {
+				update({ ...slide, state: { ...slide.state, isOpen: false } });
+			}
+		} else if (swipingDirections.includes(Direction.UP)) {
+			const slide = state[state.length - 1];
+			update({ ...slide, state: { ...slide.state, isOpen: true } });
+		}
+	};
+
 	return (
-		<>
+		<Swipable global onSwipe={onSwipe}>
 			{/* NOTE: SlidingDrawers that have been closed, remain in state array */}
 			{/* NOTE: Ordering of those slides in state might matter, once the "history" of slides needs to be accesses - e.g. open the previously closed SlidingDrawer by swiping up */}
 			{state.map((slide) => (
@@ -23,6 +41,6 @@ export const SlidingDrawerManager = () => {
 					<slide.component {...slide.props} />
 				</SlidingDrawer>
 			))}
-		</>
+		</Swipable>
 	);
 };

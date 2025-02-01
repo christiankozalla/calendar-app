4510ed4c06b2777a065ce629ae4d7124e60ac2ec
Christian Kozalla <christian.kozalla@gmail.com>
2025-02-01 23:19:23 +0100

    remove index fallback and static dir

--- main.go
+++ main.go
@@ -10,8 +10,6 @@ import (
 	_ "github.com/christiankozalla/calendar-app/migrations"
 
 	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/apis"
-	"github.com/pocketbase/pocketbase/core"
 	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
@@ -23,15 +21,6 @@ func main() {
 
 	app := pocketbase.New()
 
-	// indexFallback true is only needed when serving an SPA through pocketbase public directory. If the frontend is a React Native app, indexFallback is not needed.
-	var indexFallback bool
-	app.RootCmd.PersistentFlags().BoolVar(
-		&indexFallback,
-		"indexFallback",
-		true,
-		"fallback the request to index.html on missing static path (eg. when pretty urls are used with SPA)",
-	)
-
 	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
 		// enable auto creation of migration files when making collection changes in the Admin UI
 		Automigrate: os.Getenv("AUTOMIGRATE") == "true",
@@ -42,12 +31,6 @@ func main() {
 
 	app.OnRecordCreateRequest("events").BindFunc(eventhandlers.SanitizeDescriptionOnCreate)
 	app.OnRecordUpdateRequest("events").BindFunc(eventhandlers.SanitizeDescriptionOnUpdate)
-
-	// serves static files from the provided public dir (if exists)
-	app.OnServe().BindFunc(func(e *core.ServeEvent) error {
-		e.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), indexFallback))
-		return e.Next()
-	})
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)

9f12c37182001f2e0a118ac6fe2283fc76151348
Christian Kozalla <christian.kozalla@gmail.com>
2025-02-01 23:41:34 +0100

    replace box shadow for bottom sheet with gray border because of inconsistencies of shadows on android

--- react-native-frontend/utils/bottomsheetStyles.ts
+++ react-native-frontend/utils/bottomsheetStyles.ts
@@ -1,27 +1,23 @@
 import { StyleSheet } from "react-native";
 
 export const bottomsheetStyles = StyleSheet.create({
-    container: {
-        marginTop: 24,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: -2,
-        },
-        shadowOpacity: 0.2,
-        shadowRadius: 4.65,
-        elevation: 4,
-    },
-    paddingTop: {
-        paddingTop: 12,
-    },
-    paddingHorizontal: {
-        paddingHorizontal: 16,
-    },
-    paddingBottom: {
-        paddingBottom: 24,
-    },
-    marginHorizontal: {
-        marginHorizontal: 16
-    }
+	container: {
+		marginTop: 24,
+		borderTopLeftRadius: 16,
+		borderTopRightRadius: 16,
+		borderWidth: 1,
+		borderColor: "#e5e7eb",
+	},
+	paddingTop: {
+		paddingTop: 12,
+	},
+	paddingHorizontal: {
+		paddingHorizontal: 16,
+	},
+	paddingBottom: {
+		paddingBottom: 24,
+	},
+	marginHorizontal: {
+		marginHorizontal: 16,
+	},
 });
